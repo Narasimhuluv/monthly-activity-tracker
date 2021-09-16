2da3c2d05c219d3e3d91d45d071bd467989ddc45
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-09-16 10:01:37 +0530

    add monthly-activity-tracker

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Control from './Control'
+import Header from './Header';
+
+function App(){
+    return(
+        <>
+            <Header />
+            <main>
+                <Control />
+            </main>
+
+        </>
+    )
+}
+
+export default App;
